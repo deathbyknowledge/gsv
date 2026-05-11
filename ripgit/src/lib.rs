31a7b0a78569a1c196bfe9be48e15b658b48e538
Steve James <me@deathbyknowledge.com>
2026-05-11 23:37:56 +0200

    v0.1.5

--- ripgit/src/lib.rs
+++ ripgit/src/lib.rs
@@ -103,7 +103,7 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     Response::from_json(&serde_json::json!({
         "name": "ripgit",
-        "version": "0.1.4",
+        "version": "0.1.5",
         "description": "Git remote backed by Cloudflare Durable Objects"
     }))
 }
