aab27f8c6ea1e7b79e22d859aa8475e4b1f4bb6e
Steve James <me@deathbyknowledge.com>
2026-05-14 15:11:48 +0200

    remove doctor from builtin

--- builtin-packages/doctor/src/cli/doctor.ts
+++
@@ -1,5 +0,0 @@
-import { defineCommand } from "@gsv/package/cli";
-
-export default defineCommand(async (ctx) => {
-  await ctx.stdout.write("gsv doctor: status checks are not implemented yet\n");
-});
